07d50abdc954919536a2321650a226c7c717bec9
Ibraheem Qahtan Abdullah Hawad <40323991+barhoomGG@users.noreply.github.com>
2025-08-24 01:30:46 +0300

    Update header.js

--- components/header/header.js
+++ components/header/header.js
@@ -1,21 +1,17 @@
 // header.js
-
-// إعداد الهيدر
 document.addEventListener('DOMContentLoaded', () => {
-  const backBtn = document.getElementById("backBtn");
-  const logo = document.getElementById("logo");
+  const backBtn = document.getElementById('backBtn');
+  const logo = document.getElementById('logo');
 
   if (backBtn) {
-    backBtn.addEventListener("click", () => {
-      showNotification("جارٍ العودة إلى الصفحة الرئيسية...");
-      setTimeout(() => (window.location.href = "index.html"), 500);
+    backBtn.addEventListener('click', () => {
+      window.location.href = 'index.html';
     });
   }
 
   if (logo) {
-    logo.addEventListener("click", () => {
-      showNotification("جارٍ العودة إلى الصفحة الرئيسية...");
-      setTimeout(() => (window.location.href = "index.html"), 500);
+    logo.addEventListener('click', () => {
+      window.location.href = 'index.html';
     });
   }
 });
